6991db15594c600c7bbc641e919e65c447d34b33
zboz1 <zbozman231@gmail.com>
2026-01-13 19:14:23 -0800

    updated main.py, added functional window.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
 from constants import *
+from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()        
+
+
+
+
 
 if __name__ == "__main__":
     main()
