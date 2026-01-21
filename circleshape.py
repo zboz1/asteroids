8ac14f3b1ef1064feb3c2403421fcbddac3317ae
zboz1 <zbozman231@gmail.com>
2026-01-20 18:59:35 -0800

    added asteroids, player, and collision

---
+++ circleshape.py
@@ -0,0 +1,27 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # must override
+        pass
+
+    def update(self, dt):
+        # must override
+        pass
+
+    def collides_with(self, other):
+        distance = pygame.math.Vector2.distance_to(self.position, other.position)
+        total_radius = self.radius + other.radius
+        return distance < total_radius
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
+from player import *
+from asteroid import *
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
 
     while True:
@@ -15,6 +31,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player) == True:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
